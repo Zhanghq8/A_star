7f2a607f14eb8b40b8438b4a59e4de0c19e7a45c
Zhanghq8 <hzhang8@wpi.edu>
2019-06-19 16:51:33 -0400

    update private variables

--- A_star.h
+++ A_star.h
@@ -25,24 +25,25 @@ namespace AStar
 
     class A_Star
     {
-        public:
-            // A_star();
-            std::vector<Vec2i> path;
-            std::vector<std::vector<int> > map; 
-            bool connected_flag;
-            bool distance_flag;
-            std::vector<std::vector<int> > x_connected;
-            void set_conflag(bool connected_flag_ = false); // 0 for 8-connected, 1 for 4-connected
-            void set_disflag(bool distance_flag_ = true); // 0 for manhattan, 1 for eucilidean
-            void set_x_connected();
-            bool isValid(Vec2i coordinates_); // check if the coordinate is valid
-            bool isGoal(Vec2i source_, Vec2i goal_); // check if the coordinate is at goal pos
-            double HScore_manhattan(Vec2i source_, Vec2i goal_); // calculate the mahattan distance from current to goal 
-            double HScore_euclidean(Vec2i source_, Vec2i goal_); // calculate the euclidean distance from current to goal
-            void Generate_map(std::string path_); // generate map from a txt file;
-            void findPath(Vec2i source_, Vec2i goal_);
-            Node* findNode(std::set<Node*>& nodes_, Vec2i coordinates_);
-            void releaseNodes(std::set<Node*>& nodes_);
+    private:
+        // A_star();
+        std::vector<Vec2i> path;
+        std::vector<std::vector<int> > map; 
+        bool connected_flag;
+        bool distance_flag;
+        std::vector<std::vector<int> > x_connected;
+    public:
+        void set_conflag(bool connected_flag_ = false); // 0 for 8-connected, 1 for 4-connected
+        void set_disflag(bool distance_flag_ = true); // 0 for manhattan, 1 for eucilidean
+        void set_x_connected();
+        bool isValid(Vec2i coordinates_); // check if the coordinate is valid
+        bool isGoal(Vec2i source_, Vec2i goal_); // check if the coordinate is at goal pos
+        double HScore_manhattan(Vec2i source_, Vec2i goal_); // calculate the mahattan distance from current to goal 
+        double HScore_euclidean(Vec2i source_, Vec2i goal_); // calculate the euclidean distance from current to goal
+        void Generate_map(std::string path_); // generate map from a txt file;
+        void findPath(Vec2i source_, Vec2i goal_);
+        Node* findNode(std::set<Node*>& nodes_, Vec2i coordinates_);
+        void releaseNodes(std::set<Node*>& nodes_);
     };
 }
 
